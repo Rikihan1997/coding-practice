91fe77a8bca211616ccfa148bda8ac654b5cb48c
Rikihan1997 <a72228937@gmail.com>
2026-05-12 12:09:58 +0900

    Add files via upload

---
+++ 2026-5-12.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+int main(void) {
+    int n;
+    char s1[100][100];
+    int s2[100];
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%s %d", s1[i], &s2[i]);
+    }
+    printf("%d\n", n + 1);
+    for (int i = 0; i < n; i++) {
+        printf("%s\n%d\n", s1[i], s2[i] + 1);
+    }
+    return 0;
+}
