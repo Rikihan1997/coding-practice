6d520adc336e4365fbee7aae8535030308baa382
Rikihan1997 <a72228937@gmail.com>
2026-05-17 10:17:15 +0900

    Add files via upload

---
+++ 2026.5.17.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include<string.h>
+
+int main(void) {
+    int n;
+    char s[101];
+    char t[101];
+    scanf("%d %s %s", &n, s, t);
+    for (int i = 0; i < n; i++) {
+        if (s[i] > t[i]) {
+            printf("Yes");
+            return 0;
+        }
+        if (s[i] < t[i]) {
+            printf("No");
+            return 0;
+        }
+    }
+    return 0;
+}
