65ea3a3515551cf16923335314df13486c0bb16d
Rikihan1997 <a72228937@gmail.com>
2026-05-18 09:32:00 +0900

    Add files via upload
    c practice

---
+++ 2026.5.18.c
@@ -0,0 +1,9 @@
+﻿#include <stdio.h>
+int main(void) {
+    int s[] = { 813, 1, 2, 923874, 23648, 782356, 3256, 2342, 24324, 112 };
+    int length = sizeof(s) / sizeof(s[0]);
+    for (int i = 0;i < length; i++) {
+        printf("%d\n", s[i]);
+    }
+    return 0;
+}
