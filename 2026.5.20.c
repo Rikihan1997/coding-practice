5bfd847a0a1dc95c3fe61a5d8ba1bc8b33db651b
Rikihan1997 <a72228937@gmail.com>
2026-05-20 10:54:41 +0900

    Add files via upload

---
+++ 2026.5.20.c
@@ -0,0 +1,11 @@
+﻿#include <stdio.h>
+int main(void) {
+    char s[11][100];
+    for (int i = 0; i < 10;i++) {
+        scanf("%s", s[i]);
+    }
+    for (int i = 0; i < 10;i++) {
+        printf("%s\n", s[i]);
+    }
+    return 0;
+}
