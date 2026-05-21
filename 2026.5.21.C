10ad4fe31a6ae8d0000fbe146dda0c3ddc20cfc8
Rikihan1997 <a72228937@gmail.com>
2026-05-21 13:09:08 +0900

    Add files via upload

---
+++ 2026.5.21.C
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+#include<string.h>
+int main(void) {
+    char n[1005];
+    scanf("%s", n);
+    int len = strlen(n);
+    for (int i = 0;i < len;i++) {
+        printf("%c", n[i]);
+        if ((len - i - 1) % 3 == 0 && i != len - 1) {
+            printf(",");
+        }
+    }
+    return 0;
+}
