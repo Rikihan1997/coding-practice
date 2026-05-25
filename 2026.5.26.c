dfd597d33ecd4f4339c765b6f43d9ee0d4e7509c
Rikihan1997 <a72228937@gmail.com>
2026-05-26 00:59:45 +0900

    Add files via upload

---
+++ 2026.5.26.c
@@ -0,0 +1,35 @@
+﻿#include<stdio.h>
+int main() {
+    int m, n;
+    scanf("%d %d", &m, &n);
+    for (int i = 0; i < n;i++) {
+        for (int j = 0;j < m;j++) {
+           int high = 0;
+           if (j > 0) {
+               do {
+                   printf(" ");
+                   high++;
+               } while (high < j);
+               printf("*\n");
+           }
+           else {
+               printf("*\n");
+           }
+        }
+        for (int k = m - 2;k >= 0;k--) {
+            int high = 0;
+            if (k > 0) {
+                do {
+                    printf(" ");
+                    high++;
+                } while (high < k);
+                printf("*\n");
+            }
+            else {
+                printf("*\n");
+            }
+        }
+
+    }
+    return 0;
+}
